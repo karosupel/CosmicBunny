6f04ee96efcaf4d8cec0d126e7e0a4f7d4c257ad
karosupel <karosupel@gmail.com>
2025-08-30 19:38:45 +0200

    PLATFORM SPAWNING
    its actually working omg

--- Cosmic Bunny/Assets/PlatformSpawnerScript.cs
+++ Cosmic Bunny/Assets/PlatformSpawnerScript.cs
@@ -7,18 +7,41 @@ public class PlatformSpawnerScript : MonoBehaviour
     // Start is called before the first frame update
     public GameObject new_platform;
     public GameObject platform_prefab;
+
+    public Camera cam;
     
     void Start()
     {
+        //cam = GetComponent<Camera>();
         //na start tworze pierwsza platforme (ktora nie moze uderzyc krolika w ³eb) i nadaje jej nazwe new
-        new_platform = Instantiate(platform_prefab,new Vector3(Random.Range(-5f, -2f),-2,0), Quaternion.identity);
+        new_platform = Instantiate(platform_prefab,new Vector3(Random.Range(Random.Range(-5f,-2f),Random.Range(2f,5f)),-2,0), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //jeœli new.y + 3 jednostki do góry znajduje siê w zasiêgu kamery (bedzie potrzebna na to jakas funkcja jak nic)
         //to ma zostaæ zespawnowany nowy obiekt i teraz to on bedzie new. i tak w nieskoñczonoœæ
-        //a ju¿ w algorytmie samej platformy dodamy niszczenie ich, jeœli bêd¹ poza kamer¹. myœlê, ¿e to najbardziej optymalne
+        //a ju¿ w algorytmie samej platformy dodamy niszczenie ich, jeœli bêd¹ poza kamer¹. myœlê, ¿e to najbardziej
+        //optymalne
+
+        if (IsVisibleToCamera(new_platform.transform.position + new Vector3(0,3,0)) == true)
+        {
+            new_platform = Instantiate(platform_prefab, new Vector3(Random.Range(-5f,5f), new_platform.transform.position.y + 3, 0), Quaternion.identity);
+        }
+    }
+
+    public bool IsVisibleToCamera(Vector3 target)
+    {
+        Vector3 target_pos = cam.WorldToViewportPoint(target);
+        if (target_pos.y < 0 || target_pos.y > 1)
+        {
+            return false; //jest poza kamer¹
+        }
+        else
+        {
+            return true; //jest widoczny dla kamery
+        }
     }
 }
