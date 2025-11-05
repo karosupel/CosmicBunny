5239e73b744b2cce446514a6aeb6682ddb97aef5
karosupel <karosupel@gmail.com>
2025-11-05 13:20:50 +0100

    Dodałam deszcz meteorytów
    Powinny byc szybciej jakos oznaczane? Moze powinien byc wiekszy ekran, zeby mozna bylo zobaczyc skad nadchodza i ich uniknac bo teraz to rng, dodac zeby umierały

--- Cosmic Bunny/Assets/MeteorSpawner.cs
+++ Cosmic Bunny/Assets/MeteorSpawner.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour
 {
-    public bool SpawnMeteor = true;
+    public bool SpawnMeteor = false;
     public float CoolDown = 10;
+
+    public GameObject Meteor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +21,25 @@ public class MeteorSpawner : MonoBehaviour
         if(SpawnMeteor == true)
         {
             StartCoroutine(MeteorSpawningTimer(CoolDown));
+            SpawnMeteor = false;
         }
     }
 
     public IEnumerator MeteorSpawningTimer(float CoolDown)
     {
-        Debug.Log("Spawn Meteor");
-        SpawnMeteor = false;
         yield return new WaitForSeconds(CoolDown);
+        Debug.Log("Spawn Meteor");
+        for(int i = 0; i<5; i++)
+        {
+            Spawn();
+            yield return new WaitForSeconds(0.1f);
+        }
         SpawnMeteor = true;
+    }
+
+    public void Spawn()
+    {
+        float rand_pos_x = Random.Range(-5f, 5f);
+        Instantiate(Meteor, new Vector3(rand_pos_x, transform.position.y, 0), Quaternion.identity);
     }
 }
